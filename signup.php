789bc6b996b8ccde4e007c221792d50c7a949176
MuradCade <king12murad@gmail.com>
2023-11-10 16:20:00 +0000

    designing the ui of the webapp

---
+++ signup.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>SignUp</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+
+	</head>
+	<body>
+
+		<div class="alignment-center">
+			<div class='wraper'>
+				<h5>Create Account!</h5>
+				<p>Create perfect resumes for the modern job market.</p>
+				<!-- form input  starts here-->
+				<div class="form-group">
+				<label class="form-label">Username</label>
+				<input type="text" name="username" placeholder="Enter Username" class="form-control">		
+				</div>
+				<div class="form-group">
+				<label class="form-label">Email</label>
+				<input type="Email" name="email" placeholder="Enter Email" class="form-control">		
+				</div>
+				<div class="form-group">
+				<label class="form-label">Password</label>
+				<input type="password" name="pwd" placeholder="Enter Password" class="form-control">	
+				</div>
+				<div class="cta">
+					<button class="btn btn-primary">Signup</button>
+				</div>
+			</div>
+		</div>
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
+</html>
+
